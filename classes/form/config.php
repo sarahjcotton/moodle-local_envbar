3ccca66de430fae670a640adb2e76ca9911b341e
Suan Kan <suankan@catalyst-au.net>
2016-07-11 10:25:57 +1000

    Fix #17. Method validate() must be overriden with some logic if it is used somewhere. We don't use it in this plugin, hence removing.

--- classes/form/config.php
+++ classes/form/config.php
@@ -332,17 +332,5 @@ class config extends \moodleform {
 
         $this->add_action_buttons();
     }
-
-    /**
-     * Returns an array with fields that are invalid while creating a new QR link.
-     *
-     * @param array $data array of ("fieldname"=>value) of submitted data
-     * @param array $files array of uploaded files "element_name"=>tmp_file_path
-     * @return array of "element_name"=>"error_description" if there are errors,
-     *         or an empty array if everything is OK (true allowed for backwards compatibility too).
-     */
-    public function validation($data, $files) {
-        $errors = parent::validation($data, $files);
-    }
 }
 
