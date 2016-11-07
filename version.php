d17d8ad1cd1ce385f2459b30cec28ad68232d740
Brendan Heywood <brendan@catalyst-au.net>
2016-11-07 16:00:03 +1100

    Version bump

--- version.php
+++ version.php
@@ -29,8 +29,8 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.'); // It must be included from a Moodle page.
 }
 
-$plugin->version   = 2016102501;      // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2016102501;      // Same as version
+$plugin->version   = 2016110701;      // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2016110701;      // Same as version
 $plugin->requires  = 2014051200;      // Requires Moodle 2.7 or later.
 $plugin->component = "local_envbar";
 $plugin->maturity  = MATURITY_STABLE;
