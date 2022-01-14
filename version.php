4dcfd251dee7f51bca7d32bb298dc40e8154dac4
Anupama Dharmajan <anupamadharmajan@catalyst-au.net>
2022-01-14 14:11:55 +1100

    Update readme file and bump version number

--- version.php
+++ version.php
@@ -29,8 +29,9 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.'); // It must be included from a Moodle page.
 }
 
-$plugin->version   = 2021011911;      // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = 2021011910;      // Same as version
+$plugin->version   = 2022011400;      // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = 2022011400;      // Same as version
 $plugin->requires  = 2014051200;      // Requires Moodle 2.7 or later.
 $plugin->component = "local_envbar";
 $plugin->maturity  = MATURITY_STABLE;
+$plugin->supported = [310, 311];      // A range of branch numbers of supported moodle versions.
